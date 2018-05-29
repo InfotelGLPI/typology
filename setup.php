42c96b379d85822ab6747a2e5537d8f93a82a792
keha <kevin.hainry@infotel.com>
2018-05-29 11:20:20 +0200

    Update setup file

--- setup.php
+++ setup.php
@@ -72,7 +72,7 @@ function plugin_version_typology() {
 
    return  [
       'name'           => _n('Typology', 'Typologies', 2, 'typology'),
-      'version'        => '2.4.0',
+      'version'        => '2.5.0',
       'author'         => "<a href='http://infotel.com/services/expertise-technique/glpi/'>Infotel</a>",
       'license'        => 'GPLv2+',
       'homepage'       => 'https://github.com/InfotelGLPI/typology',
